708672e0c35c6c8378000bac2fd3e3feaa89e24d
Marcello Bastea-Forte <marcello@cellosoft.com>
2014-03-24 21:21:58 -0700

    make index.html have different messages depending how the page is opened

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,5 @@
 console.log("Hello world!");
 
 require('./lib/foobar');
+
+document.getElementById('message').textContent = location.hostname ? 'Congratulations, you got everything running!' : 'You are looking at the pre-compiled website!'
